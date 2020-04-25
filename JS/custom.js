c68b5ca1922ec3c15aa42883def99cad73a813f9
Werka <weronika.gajska@gmail.com>
2020-04-25 19:36:59 +0200

    no message

---
+++ JS/custom.js
@@ -0,0 +1,32 @@
+// WOW ANIMATION
+
+$(function () {
+    new WOW().init();
+})
+
+//   NAVIGATION
+$(function () {
+    $(window).scroll(function () {
+        if($(this).scrollTop() < 50){
+            //hide navbar
+            $("nav").removeClass("werka-top-navbar");
+            $("#back-to-top").fadeOut();
+        } else {
+            //show nav
+            $("nav").addClass("werka-top-navbar");
+            $("#back-to-top").fadeIn();
+        }
+    })
+});
+
+//  SMOOOTH SCROOLING
+$(function () {
+    $("a.smooth-scroll").click(function (event) {
+        event.preventDefault();
+        // get/return id like #about if is clicked
+        var section = $(this).attr("href");
+        $('html, body').animate({
+            scrollTop: $(section).offset().top
+        }, 1250), "easeInOutExpo";
+    });
+})
